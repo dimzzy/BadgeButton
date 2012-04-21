882848b35087618c8668d8ac2ddcf3c824aaf46f
Dmitry Stadnik <dimzzy@gmail.com>
2012-04-21 09:48:57 +0200

    Initial Commit

---
+++ BadgeButton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BadgeButton
+//
+//  Created by Dmitry Stadnik on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
