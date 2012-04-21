387da5db5a703ae7d69402aebcb0badc60eaef60
Dmitry Stadnik <dimzzy@gmail.com>
2012-04-21 20:22:49 +0200

    Add content

--- BadgeButton/ViewController.h
+++ BadgeButton/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property IBOutlet UITableView *tableView;
 
 @end
--- BadgeButton/ViewController.m
+++ BadgeButton/ViewController.m
@@ -8,27 +8,71 @@
 
 #import "ViewController.h"
 
+static const NSInteger kBadgeButtonTag = 34574546234;
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+@synthesize tableView = _tableView;
 
-- (void)viewDidUnload
-{
+- (void)viewDidUnload {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
+	self.tableView = nil;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return 12;
+}
+
+- (UITableViewCell *)makeBadgeCell:(UITableView *)tableView withCount:(NSUInteger)count {
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BadgeCell"];
+	UIButton *badgeButton;
+	if (!cell) {
+		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+									  reuseIdentifier:@"BadgeCell"];
+		UIImage *bgImage = [UIImage imageNamed:@"cell-bg.png"];
+		UIImageView *bgView = [[UIImageView alloc] initWithImage:bgImage];
+		bgView.contentMode = UIViewContentModeScaleToFill;
+		cell.backgroundView = bgView;
+		UIImage *hlImage = [UIImage imageNamed:@"cell-bg-hl.png"];
+		UIImageView *hlView = [[UIImageView alloc] initWithImage:hlImage];
+		hlView.contentMode = UIViewContentModeScaleToFill;
+		cell.selectedBackgroundView = hlView;
+		
+		cell.textLabel.textColor = [UIColor whiteColor];
+		cell.textLabel.backgroundColor = [UIColor clearColor];
+		cell.textLabel.opaque = NO;
+		cell.textLabel.font = [UIFont boldSystemFontOfSize:16];
+		cell.textLabel.text = @"Text";
+		
+		badgeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+		badgeButton.tag = kBadgeButtonTag;
+		UIImage *badgeImage = [[UIImage imageNamed:@"button-badge.png"] stretchableImageWithLeftCapWidth:15
+																							topCapHeight:10];
+		[badgeButton setBackgroundImage:badgeImage forState:UIControlStateNormal];
+		badgeButton.adjustsImageWhenHighlighted = NO;
+		badgeButton.contentEdgeInsets = UIEdgeInsetsMake(1, 8, 1, 8);
+		[badgeButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+		badgeButton.titleLabel.font = [UIFont boldSystemFontOfSize:14];
+		badgeButton.titleLabel.textAlignment = UITextAlignmentCenter;
+		[cell.contentView addSubview:badgeButton];
+	} else {
+		badgeButton = (UIButton *)[cell.contentView viewWithTag:kBadgeButtonTag];
+	}
+	[badgeButton setTitle:[NSString stringWithFormat:@"%d", count] forState:UIControlStateNormal];
+	[badgeButton sizeToFit];
+	CGRect badgeRect = badgeButton.frame;
+	badgeRect.origin.x = 300 - badgeRect.size.width;
+	badgeRect.origin.y = (44 - badgeRect.size.height) / 2;
+	badgeButton.frame = badgeRect;
+	return cell;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	return [self makeBadgeCell:tableView withCount:(indexPath.row * indexPath.row)];
 }
 
 @end
